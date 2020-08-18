bfe935219bcb3d5b516f5fccd4a7703150b4030a
vovinov <v.v.ovinov@yandex.ru>
2020-08-18 19:10:38 +0300

    add grid & title

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="wrapper">
+      <section className="cat-food">
+        <div className="_container">
+          <h1 className="cat-food__title">Ты сегодня кормил кота?</h1>
+          <div className="cat-food__cards">
+            <div className="cat-card"></div>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
