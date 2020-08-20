94886ceb5e81d7c83fe8f2597b26338b050dcef3
vovinov <v.v.ovinov@yandex.ru>
2020-08-20 19:50:00 +0300

    Updates

---
+++ precache-manifest.efd380aaadcc82dbc3c63b65910acdf7.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51027835d1db52ffbe22a5f72543bcc8",
+    "url": "/test-funbox/index.html"
+  },
+  {
+    "revision": "e8cca32be37440be8213",
+    "url": "/test-funbox/static/css/main.d6d3fe75.chunk.css"
+  },
+  {
+    "revision": "fba5358a0622962af31f",
+    "url": "/test-funbox/static/js/2.4b947310.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/test-funbox/static/js/2.4b947310.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8cca32be37440be8213",
+    "url": "/test-funbox/static/js/main.a30f73b4.chunk.js"
+  },
+  {
+    "revision": "7f3f8ed2c5d7bbc26477",
+    "url": "/test-funbox/static/js/runtime-main.2fc311a1.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.44bde874.svg"
+  },
+  {
+    "revision": "5482528bdaa03c71301df260505d6fb0",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.5482528b.eot"
+  },
+  {
+    "revision": "c33b184f5dd1056361d472fd0a43e6a5",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.c33b184f.woff"
+  },
+  {
+    "revision": "ecd3f3edfa509aafe896b9d03bffff82",
+    "url": "/test-funbox/static/media/Exo2.0-Thin.ecd3f3ed.ttf"
+  },
+  {
+    "revision": "61122bf8c47c4c3d4722ea7825d221df",
+    "url": "/test-funbox/static/media/cat.61122bf8.png"
+  },
+  {
+    "revision": "7b91c5fa6af2172f662b68332d3cccac",
+    "url": "/test-funbox/static/media/trebuc.7b91c5fa.ttf"
+  },
+  {
+    "revision": "978ae894f6d8d922f58063bb595f27a9",
+    "url": "/test-funbox/static/media/trebuc.978ae894.svg"
+  },
+  {
+    "revision": "c614fd54005a322d1d7ff4576638dfdc",
+    "url": "/test-funbox/static/media/trebuc.c614fd54.woff"
+  },
+  {
+    "revision": "e1306853d6ec7dccb830d0424aa7c827",
+    "url": "/test-funbox/static/media/trebuc.e1306853.eot"
+  },
+  {
+    "revision": "36fc883c09c40c62c0c4614af27ec623",
+    "url": "/test-funbox/static/media/trebucbd.36fc883c.woff"
+  },
+  {
+    "revision": "525cdb1bcfd19b8d7c89bbf2d04302c6",
+    "url": "/test-funbox/static/media/trebucbd.525cdb1b.eot"
+  },
+  {
+    "revision": "6bf791fb866de02766a97b57c33ccd7f",
+    "url": "/test-funbox/static/media/trebucbd.6bf791fb.svg"
+  },
+  {
+    "revision": "c93facaf3cd656fb21693233ba451d3c",
+    "url": "/test-funbox/static/media/trebucbd.c93facaf.ttf"
+  }
+]);
